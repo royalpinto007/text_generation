2fc751c63c1e067939f01bc7431cc1dc76ffdb8c
Royal Simpson Pinto <royalpinto007@gmail.com>
2024-08-12 16:18:41 +0530

    feat: phpcs workflow
    Signed-off-by: Royal Simpson Pinto <royalpinto007@gmail.com>

--- src/Controller/TextGenerationController.php
+++ src/Controller/TextGenerationController.php
@@ -2,24 +2,39 @@
 
 namespace Drupal\text_generation\Controller;
 
+use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Drupal\Core\Controller\ControllerBase;
 use function Codewithkyrian\Transformers\Pipelines\pipeline;
 
+/**
+ * Defines the Text Generation Controller.
+ */
 class TextGenerationController extends ControllerBase {
-    public function generateText(Request $request) {
-        // Decode JSON from the request body
-        $data = json_decode($request->getContent(), true);
-        $text = $data['text'] ?? null;  // Use null coalescing operator to handle absent key
-        
-        if (empty($text)) {
-            return new JsonResponse(['error' => 'No text provided'], 400);
-        }
 
-        $generator = pipeline('text-generation', 'Xenova/gpt2');
-        $output = $generator($text);
+  /**
+   * Generates text using a machine learning model.
+   *
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *   The current request object, containing JSON data.
+   *
+   * @return \Symfony\Component\HttpFoundation\JsonResponse
+   *   A JSON response with the generated text or an error message.
+   */
+  public function generateText(Request $request) {
+    // Decode JSON from the request body.
+    $data = json_decode($request->getContent(), TRUE);
+    // Use null coalescing operator to handle absent key.
+    $text = $data['text'] ?? NULL;
 
-        return new JsonResponse(['generated_text' => $output[0]['generated_text']]);
+    if (empty($text)) {
+      return new JsonResponse(['error' => 'No text provided'], 400);
     }
+
+    $generator = pipeline('text-generation', 'Xenova/gpt2');
+    $output = $generator($text);
+
+    return new JsonResponse(['generated_text' => $output[0]['generated_text']]);
+  }
+
 }
